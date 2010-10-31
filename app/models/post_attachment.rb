0ed0bc783f258a1828df907f9f3cdd216fc97100
Ryan Ferretti <ryanferretti@gmail.com>
2010-10-31 15:10:55 -0500

    switched photos for posts to s3

--- app/models/post_attachment.rb
+++ app/models/post_attachment.rb
@@ -1,6 +1,13 @@
 class PostAttachment < ActiveRecord::Base
   belongs_to :post
+
   has_attached_file :photo, :styles => { :large => "900x600>", :small => "85x85>" },
-                    :url  => "/assets/photos/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/photos/:id/:style/:basename.:extension"
+                    :storage => ENV['S3_BUCKET'] ? :s3 : :filesystem,
+                    :s3_credentials => {
+                      :access_key_id => ENV['S3_KEY'],
+                      :secret_access_key => ENV['S3_SECRET']
+                    },
+                    :bucket => ENV['S3_BUCKET'],
+                    :path => ENV['S3_BUCKET'] ? ":class/:id/:style/:filename" : ":rails_root/public/assets/photos/:id/:style/:basename.:extension",
+                    :url => "/assets/photos/:id/:style/:basename.:extension"
 end
